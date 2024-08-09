1f3edd09e146c3027ebc4253b20441076ff392aa
Redact <68514509+evoredact@users.noreply.github.com>
2024-08-09 14:44:49 +0800

    Add files via upload

--- MyFirstPlugin/Features/FakeLag.cs
+++ MyFirstPlugin/Features/FakeLag.cs
@@ -5,10 +5,10 @@ namespace VortexClient.Features;
 internal class FakeLag : MonoBehaviour {
     public static new bool enabled = false;
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Z)) {
-            enabled = !enabled;
-            Plugin.Log.LogWarning($"FakeLag State is now: {enabled}");
-        }
-    }
+    //void Update() {
+    //    if (Input.GetKeyDown(KeyCode.Z)) {
+    //        enabled = !enabled;
+    //        Plugin.Log.LogWarning($"FakeLag State is now: {enabled}");
+    //    }
+    //}
 }
